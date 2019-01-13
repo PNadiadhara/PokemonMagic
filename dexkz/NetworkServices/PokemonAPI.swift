2bc3c0f3cbb23dff21a1b4a391a8a8968a197bcf
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-01-12 21:14:54 -0500

    project has pokemon data, first and detailed view controllers. Need to format transparent button for detailed view.

--- dexkz/NetworkServices/PokemonAPI.swift
+++ dexkz/NetworkServices/PokemonAPI.swift
@@ -9,26 +9,27 @@
 import Foundation
 
 final class PokemonAPI {
-    
-    static func searchPokemon(keyword: String, completionHandler: @escaping (AppError?, [Pokemon]?) -> Void) {
-        NetworkHelper.shared.performDataTask(endpointURLString: "https://api.pokemontcg.io/v1/cards?contains=imageUrl,imageUrlHiRes,attacks") { (appError, data, httpResponse) in
-            if let appError = appError {
-                completionHandler(appError, nil)
-            }
-            guard let response = httpResponse,
-                (200...299).contains(response.statusCode) else {
-                    let statusCode = httpResponse?.statusCode ?? -999
-                    completionHandler(AppError.badStatusCode(String(statusCode)), nil)
-                    return
-            }
-            if let data = data {
-                do {
-                    let pokemonData = try JSONDecoder().decode([Pokemon].self, from: data)
-                    completionHandler(nil, pokemonData)
-                } catch {
-                    completionHandler(AppError.decodingError(error), nil)
-                }
-            }
+  
+  static func searchPokemon(completionHandler: @escaping (AppError?, [PokemonInfo]?) -> Void) {
+    NetworkHelper.shared.performDataTask(endpointURLString: "https://api.pokemontcg.io/v1/cards?contains=imageUrl,imageUrlHiRes,attacks") { (appError, data, httpResponse) in
+      if let appError = appError {
+        completionHandler(appError, nil)
+      }
+      guard let response = httpResponse,
+        (200...299).contains(response.statusCode) else {
+          let statusCode = httpResponse?.statusCode ?? -999
+          completionHandler(AppError.badStatusCode(String(statusCode)), nil)
+          return
+      }
+      if let data = data {
+        do {
+          let pokemonData = try JSONDecoder().decode(Pokemon.self, from: data)
+          let cards = pokemonData.cards
+          completionHandler(nil, cards)
+        } catch {
+          completionHandler(AppError.decodingError(error), nil)
         }
+      }
     }
+  }
 }
--- dexkz/View/PokemonViewCell.swift
+++ dexkz/View/PokemonViewCell.swift
@@ -12,5 +12,60 @@ class PokemonViewCell: UICollectionViewCell {
 
    
     @IBOutlet weak var pokemonImage: UIImageView!
+  
+  
+  private var pokemon = [PokemonInfo]() {
+    didSet{
+      DispatchQueue.main.async {
+        self.pokemonCollectionView.reloadData()
+      }
+    }
+  }
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    pokemonCollectionView.dataSource = self
+    pokemonCollectionView.delegate = self
+    getPokemonDataFromAPI()
     
+  }
+  
+  
+  private func getPokemonDataFromAPI(){
+    PokemonAPI.searchPokemon{ (appError, pokemon) in
+      if let appError = appError {
+        print(appError.errorMessage())
+      } else if let pokemon = pokemon {
+        self.pokemon = pokemon
+        dump(pokemon)
+      }
+    }
+  }
+  
+  
+}
+
+extension PokemonViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return pokemon.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = pokemonCollectionView.dequeueReusableCell(withReuseIdentifier: "PokemonCell", for: indexPath) as? PokemonViewCell else {return UICollectionViewCell()}
+    
+    let currentPokemon = pokemon[indexPath.item]
+    
+    cell.setUpImage(pokemon: currentPokemon)
+    
+    return cell
+    
+  }
+  
+}
+
+extension PokemonViewController: UICollectionViewDelegate {
+  func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 250
+  }
 }
